b446e2bac98cb49fe5160f24025b6c24f0142dce
SCSS77 <scastro@emagister.com>
2024-03-21 10:57:34 +0100

    Podcast api: adjustment test files and plugins

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: ['./**/*.test.ts', './**/*.test.tsx'],
-    globals: true
+    environment: 'jsdom'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
