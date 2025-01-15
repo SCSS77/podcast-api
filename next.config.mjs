6f6610f327d91ed2ccc6284ff76bca84b09226e9
SCSS77 <sandra@scss-studio.com>
2025-01-15 18:52:09 +0100

    new adjustments

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**'
-      }
-    ]
-  }
+        hostname: '**',
+      },
+    ],
+  },
+};
 
-}
-
-export default nextConfig
+export default nextConfig;
